6e4178b20959e1a0285ed5b5e371662d0c36190d
Abraham Mendoza <80663373+Estaticly@users.noreply.github.com>
2023-02-13 13:30:40 -0800

    Proj 0 update 2_13
    project is working as it should

--- 522/Proj 0/Proj 0/Proj 0.ino
+++ 522/Proj 0/Proj 0/Proj 0.ino
@@ -1,64 +1,54 @@
 
-
-
-const int buttonPin = 2;  //button attached on pin #2
-const int ledPin = 4;    // led light attached to pin #4
-
-bool isOn =0;
-
-int period=0;
-int currentPeriod;
-int startPeriod;
-
+//pins
+const int buttonPin = 2;  //button on pin 2
+const int ledPin = 4;    // led light on pin 4
+//time
+int period=500;//intial period of time
+int currentPeriod;//the curren period of time
+int startPeriod;//the start of the period of time
+//button state
 int buttonCou = 0;  //counts the number of times the button is pressed
 int buttonState = 0;        // current state of the button
 int lastButtonState = 0;    // previous state of the button
 
 void setup() {
-  // initialize the button pin as a input:
-  pinMode(buttonPin, INPUT);
-  // initialize the LED as an output:
-  pinMode(ledPin, OUTPUT);
-  // initialize serial communication:
+  pinMode(buttonPin, INPUT);//button set as a input
+  
+  pinMode(ledPin, OUTPUT); //led set as a output
 
-  startPeriod = millis();
+  startPeriod = millis();//set the start period 
 }
 
 
 void loop() {
-  // read the pushbutton input pin:
+  // read button input pin:
   buttonState = digitalRead(buttonPin);
   if(buttonState != lastButtonState){
     if(buttonState ==HIGH){
       buttonCou++;
-    
-      //digitalWrite(ledPin, HIGH);
     }
-    delay(50);
-  }//I have something going i think the delays are messing me up as it doesnt change modes unles i hold it down or time it with the lights 
+    delay(50);//delay so button wont bounce
+  }
   lastButtonState = buttonState;
   currentPeriod = millis();
+
   //sets up the frequency the light turns on and off
   
-
   if(buttonCou ==1){
-    period = 1000;
-    isOn=1;
+    period = 250;//2hz
   }else if(buttonCou ==2) {
-    period = 500;
+    period = 125;//4hz
   }else if(buttonCou ==3){
-    period = 250;
+    period = 63;//8hz
   }else if(buttonCou ==4){
-    period = 125;
-  }else if(buttonCou >= 5){
-    digitalWrite(ledPin, LOW);
-    buttonCou = 0;
-    isOn =0;
+    buttonCou = 0;//reset
+    period = 500;//1hz
   }
-
-  if(currentPeriod-startPeriod >= period && isOn == 1){
+    
+  //activates led at time interval chosen
+  if(currentPeriod-startPeriod >= period){
     digitalWrite(ledPin, !digitalRead(ledPin));
     startPeriod = currentPeriod;
   }
-  //digitalWrite(ledPin, LOW);
+  
 }
