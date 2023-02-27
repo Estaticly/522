a319c111165ccdace7e36d8e93b7213d86da83bb
Abraham Mendoza <80663373+Estaticly@users.noreply.github.com>
2023-02-27 14:23:34 -0800

    Create proj1.ino

---
+++ 522/Proj 1/proj1/proj1.ino
@@ -0,0 +1,32 @@
+
+int PotentioPin = 0;//potentiometer pin A0
+
+int ledPin = 3;//LED pin ~3
+
+int inputVal = 0;//Stores the val from pin A0 from 0-1023
+
+int outputVal = 0;//Stores the output of A0 from 0-255
+
+/******************************************/
+
+void setup(){
+
+  Serial.begin(9600);//set the serial communication baudrate as 9600
+
+}
+
+void loop(){
+
+  inputVal = analogRead(PotentioPin);//Read val from the potentiometer
+
+  Serial.println(inputVal); //print the input val
+
+  outputVal = map(inputVal, 0, 1023, 0, 255); //Converts val 0-1023  to val 0-255
+
+  Serial.println(outputVal); //print the output val
+
+  analogWrite(ledPin, outputVal); //Turns led on with the coresponding output val from 0-255
+
+}
+
+/*******************************************/
