9406a01c7b72776ad37065c46e2769fb4c4673f6
Abraham Mendoza <80663373+Estaticly@users.noreply.github.com>
2023-02-28 11:32:31 -0800

    Update proj1.ino

--- 522/Proj 1/proj1/proj1.ino
+++ 522/Proj 1/proj1/proj1.ino
@@ -1,4 +1,4 @@
-
+//Abraham Mendoza
 int PotentioPin = 0;//potentiometer pin A0
 
 int ledPin = 3;//LED pin ~3
@@ -7,16 +7,15 @@ int inputVal = 0;//Stores the val from pin A0 from 0-1023
 
 int outputVal = 0;//Stores the output of A0 from 0-255
 
-/******************************************/
-
 void setup(){
+  pinMode(PotentioPin,INPUT);
+
+  pinMode(ledPin,OUTPUT);
 
   Serial.begin(9600);//set the serial communication baudrate as 9600
-
 }
 
 void loop(){
-
   inputVal = analogRead(PotentioPin);//Read val from the potentiometer
 
   Serial.println(inputVal); //print the input val
@@ -26,7 +25,4 @@ void loop(){
   Serial.println(outputVal); //print the output val
 
   analogWrite(ledPin, outputVal); //Turns led on with the coresponding output val from 0-255
-
 }
-
-/*******************************************/
